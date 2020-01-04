6d13e08da7ff6e2a528ff53dc5eda319305631c0
unknown <shabankhawar99@gmail.com>
2020-01-05 02:05:01 +0500

    its ya boi back at it again stuck with css

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Navbar from "./indexComponents/Navbar";
+import NavBar from "./indexComponents/Navbar";
 
 const Index = () => {
-  return <Navbar />;
+  return <NavBar />;
 };
 
 export default Index;
--- src/components/indexComponents/Navbar.js
+++ src/components/indexComponents/Navbar.js
@@ -7,35 +7,36 @@ import {
   Nav,
   NavItem,
   NavLink,
-  NavbarText,
   Button
 } from "reactstrap";
 
-const Example = props => {
+const NavBar = props => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
-
+  const googleAuthLink = "http://localhost:5000/auth/google";
   return (
     <div>
-      <Navbar color="secondary" light expand="md">
+      <Navbar className="navi" light expand="md">
         <NavbarBrand href="/">soundLoud</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
+          <Nav className="mr-auto naver" navbar>
+            <NavItem className="navItem">
+              <NavLink href={googleAuthLink}>
+                <Button className="button">Sign In</Button>
+              </NavLink>
             </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/ShabanK">GitHub</NavLink>
+            <NavItem className="navItem">
+              <NavLink href={googleAuthLink}>
+                <Button className="button">Sign Up</Button>
+              </NavLink>
             </NavItem>
           </Nav>
-          <Button>Sign In</Button>
-          <Button>Sign Up</Button>
         </Collapse>
       </Navbar>
     </div>
   );
 };
 
-export default Example;
+export default NavBar;
